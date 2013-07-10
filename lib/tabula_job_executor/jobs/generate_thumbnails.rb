d8f4a3a67ba162959d5fe5e23df7c3867e72cda7
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2013-07-10 12:44:52 -0400

    fix status messages for table detection

--- lib/tabula_job_executor/jobs/generate_thumbnails.rb
+++ lib/tabula_job_executor/jobs/generate_thumbnails.rb
@@ -2,7 +2,7 @@ require_relative '../executor.rb'
 require_relative '../../thumbnail_generator.rb'
 
 class GenerateThumbnailJob < Tabula::Background::Job
-  # args: (:file, :output_dir, :thumbnail_sizes, :page_index_job)
+  # args: (:file, :output_dir, :thumbnail_sizes, :page_index_job_uuid)
 
   def perform
 
@@ -11,8 +11,8 @@ class GenerateThumbnailJob < Tabula::Background::Job
     file = options[:file]
     output_dir = options[:output_dir]
     thumbnail_sizes = options[:thumbnail_sizes]
-    page_index_job = options[:page_index_job]
-    table_detection_job = options[:table_detection_job]
+    page_index_job_uuid = options[:page_index_job_uuid]
+    detect_tables_job_uuid = options[:detect_tables_job_uuid]
 
     # return some status to browser
     at(0, 100, "generating page thumbnails...")
@@ -20,22 +20,22 @@ class GenerateThumbnailJob < Tabula::Background::Job
     generator.add_observer(self, :at)
     generator.generate_thumbnails!
 
-    unless table_detection_job.nil?
-      while !Tabula::Background::JobExecutor.get(table_detection_job).completed? do
-        at(50, 100, "auto-detecting tables...",
+    unless detect_tables_job_uuid.nil?
+      detect_tables_job = Tabula::Background::JobExecutor.get(detect_tables_job_uuid)
+      while !detect_tables_job.completed? do
+        at(detect_tables_job.status['num'], detect_tables_job.status['total'], "auto-detecting tables...",
            )
         sleep 0.25
       end
     end
 
-    while !Tabula::Background::JobExecutor.get(page_index_job).completed? do
+    while !Tabula::Background::JobExecutor.get(page_index_job_uuid).completed? do
       at(99, 100, "generating page thumbnails...",
          )
       sleep 0.25
     end
 
-    at(100, 100, "complete",
-       )
+    at(100, 100, "complete" )
 
   end
 end
