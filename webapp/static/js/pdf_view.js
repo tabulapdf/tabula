34a06e06a9191eb2cf4ad8b0aa0ebeb50f436cae
Manuel Aristarán <jazzido@jazzido.com>
2013-11-22 19:10:37 -0300

    reflecting jazzido/tabula-extractor@b7350346a1030946590b9de638ebe35642dc621db7350346a1030946590b9de638ebe35642dc621d

--- webapp/static/js/pdf_view.js
+++ webapp/static/js/pdf_view.js
@@ -164,41 +164,6 @@ Tabula.PDFView = Backbone.View.extend({
               }, this));
     },
 
-    debugColumns: function(image) {
-        image = $(image);
-        var imagePos = image.offset();
-        var newCanvas =  $('<canvas/>',{'class':'debug-canvas'})
-            .attr('width', image.width())
-            .attr('height', image.height())
-            .css('top', imagePos.top + 'px')
-            .css('left', imagePos.left + 'px');
-        $('body').append(newCanvas);
-
-        var thumb_width = $(image).width();
-        var thumb_height = $(image).height();
-        var pdf_width = parseInt($(image).data('original-width'));
-        var pdf_height = parseInt($(image).data('original-height'));
-        var pdf_rotation = parseInt($(image).data('rotation'));
-
-        var scale_x = (thumb_width / pdf_width);
-        var scale_y = (thumb_height / pdf_height);
-
-        $.get('/debug/' + this.PDF_ID + '/columns',
-              this.lastQuery,
-              _.bind(function(data) {
-                  $.each(data, _.bind(function(i, column) {
-                      $(newCanvas).drawRect({
-                          x: column.left * scale_x,
-                          y: this.lastSelection.y1,
-                          width: (column.right - column.left) * scale_x,
-                          height: this.lastSelection.y2 - this.lastSelection.y1,
-                          strokeStyle: this.colors[i % this.colors.length],
-                          fromCenter: false
-                      });
-                  }, this));
-              }, this) );
-    },
-
     debugCharacters: function(image) {
       image = $(image);
       var imagePos = image.offset();
