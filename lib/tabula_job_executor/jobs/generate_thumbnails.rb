a4cf6c2568ecf0097fff7c6813ce9060738a4c1f
Manuel Aristarán <jazzido@jazzido.com>
2014-01-27 17:36:10 -0300

    closing #124

--- lib/tabula_job_executor/jobs/generate_thumbnails.rb
+++ lib/tabula_job_executor/jobs/generate_thumbnails.rb
@@ -11,31 +11,10 @@ class GenerateThumbnailJob < Tabula::Background::Job
     file = options[:file]
     output_dir = options[:output_dir]
     thumbnail_sizes = options[:thumbnail_sizes]
-    page_index_job_uuid = options[:page_index_job_uuid]
-    detect_tables_job_uuid = options[:detect_tables_job_uuid]
 
-    # return some status to browser
-    at(0, 100, "generating page thumbnails...")
     generator = JPedalThumbnailGenerator.new(file, output_dir, thumbnail_sizes)
     generator.add_observer(self, :at)
     generator.generate_thumbnails!
-
-    unless detect_tables_job_uuid.nil?
-      detect_tables_job = Tabula::Background::JobExecutor.get(detect_tables_job_uuid)
-      while !detect_tables_job.completed? do
-        at(detect_tables_job.status['num'], detect_tables_job.status['total'], "auto-detecting tables...",
-           )
-        sleep 0.25
-      end
-    end
-
-    while !Tabula::Background::JobExecutor.get(page_index_job_uuid).completed? do
-      at(99, 100, "generating page thumbnails...",
-         )
-      sleep 0.25
-    end
-
-    at(100, 100, "complete" )
 
   end
 end
