cd4d60854c27a132ed50d054ce7ec51561aa0323
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2015-01-24 17:37:36 -0500

    revise selections button works

--- webapp/static/js/tabula.js
+++ webapp/static/js/tabula.js
@@ -23,7 +23,7 @@ var TabulaRouter = Backbone.Router.extend({
       }
     })
 
-    $('#tabula').html( _.template( $('#upload-template').html().replace(/nestedscript/g, 'script') )({
+    $('#tabula-app').html( _.template( $('#upload-template').html().replace(/nestedscript/g, 'script') )({
       TABULA_VERSION: TABULA_VERSION,
       pct_complete: 0,
       importing: false
@@ -36,12 +36,12 @@ var TabulaRouter = Backbone.Router.extend({
   //   $('body').prepend( _.template( $('#navbar-template').html() )({}) ); // navbar.
   //   //TODO: there's another errorMsg: "Sorry, your file upload could not be processed. Please double-check that the file you uploaded is a valid PDF file and try again."
   //   var errorMsg = "Sorry, the file you uploaded was not detected as a PDF. You must upload a PDF file. <a href='/'>Please try again</a>.";
-  //   $('#tabula').html( _.template( $('#upload-error-template').html() )({message: errorMsg}) );
+  //   $('#tabula-app').html( _.template( $('#upload-error-template').html() )({message: errorMsg}) );
   // },
 
   // status: function(file_id){
   //   $('body').prepend( _.template( $('#navbar-template').html() )({}) ); // navbar.
-  //   $('#tabula').html( _.template( $('#upload-status-template').html() )({}) );
+  //   $('#tabula-app').html( _.template( $('#upload-status-template').html() )({}) );
   //   $.ajax({
   //     url: "/js/upload_status.js",
   //     dataType: "script",
@@ -58,7 +58,7 @@ var TabulaRouter = Backbone.Router.extend({
   view: function(file_id) {
     $('body').prepend( _.template( $('#navbar-template').html().replace(/nestedscript/g, 'script') )({}) ); // navbar.
     $('body').addClass('page-selections')
-    $('#tabula').html( _.template( $('#pdf-view-template').html().replace(/nestedscript/g, 'script') )({}) );
+    $('#tabula-app').html( _.template( $('#pdf-view-template').html().replace(/nestedscript/g, 'script') )({}) );
 
     $.ajax({
       url: "/js/pdf_view.js",
@@ -76,7 +76,7 @@ var TabulaRouter = Backbone.Router.extend({
   // extract: function(file_id, ) {
   //   $('body').prepend( _.template( $('#navbar-template').html().replace(/nestedscript/g, 'script') )({}) ); // navbar.
   //   $('body').addClass('page-export')
-  //   $('#tabula').html( _.template( $('#page-export-template').html().replace(/nestedscript/g, 'script') )({}) );
+  //   $('#tabula-app').html( _.template( $('#page-export-template').html().replace(/nestedscript/g, 'script') )({}) );
   
   //   $.ajax({
   //     url: "/js/pdf_extract.js",
