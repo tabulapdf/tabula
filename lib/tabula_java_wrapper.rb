c3fb6f34ff1932074ccd9fd7b38271b07434e5a1
Manuel Aristarán <jazzido@jazzido.com>
2015-03-11 23:24:50 -0400

    remove dead code

--- lib/tabula_java_wrapper.rb
+++ lib/tabula_java_wrapper.rb
@@ -65,62 +65,6 @@ module Tabula
 
   end
 
-  def Tabula.extract_table(pdf_path, page, area, options={})
-    options = {
-      :password => '',
-      :detect_ruling_lines => true,
-      :vertical_rulings => [],
-      :extraction_method => "guess",
-    }.merge(options)
-
-    if page.is_a?(Integer)
-      page = [page.to_java(:int)]
-    end
-
-    extractor = Extraction::ObjectExtractor.new(pdf_path,
-                                                options[:password])
-
-    pdf_page = extractor.extract(page).next
-    extractor.close!
-
-    if ["spreadsheet", "original"].include? options[:extraction_method]
-      use_spreadsheet_extraction_method = options[:extraction_method] == "spreadsheet"
-    else
-      use_spreadsheet_extraction_method = pdf_page.is_tabular?
-    end
-
-    if use_spreadsheet_extraction_method
-      spreadsheets = pdf_page.get_area(area).spreadsheets
-      return spreadsheets.empty? ? Spreadsheet.empty(pdf_page) : spreadsheets#.inject(&:+)
-    end
-
-    use_detected_lines = false
-    if options[:detect_ruling_lines] && options[:vertical_rulings].empty?
-
-      detected_vertical_rulings = Ruling.crop_rulings_to_area(pdf_page.vertical_ruling_lines,
-                                                              area)
-
-
-      # only use lines if at least 80% of them cover at least 90%
-      # of the height of area of interest
-
-      # TODO this heuristic SUCKS
-      # what if only a couple columns is delimited with vertical rulings?
-      # ie: https://www.dropbox.com/s/lpydler5c3pn408/S2MNCEbirdisland.pdf (see 7th column)
-      # idea: detect columns without considering rulings, detect vertical rulings
-      # calculate ratio and try to come up with a threshold
-      use_detected_lines = detected_vertical_rulings.size > 2 \
-      && (detected_vertical_rulings.count { |vl|
-            vl.height / area.height > 0.9
-          } / detected_vertical_rulings.size.to_f) >= 0.8
-
-    end
-
-    pdf_page
-      .get_area(area)
-      .get_table(:vertical_rulings => use_detected_lines ? detected_vertical_rulings.subList(1, detected_vertical_rulings.size) : options[:vertical_rulings])
-
-  end
 
   module Extraction
 
