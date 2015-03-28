cd0221d0e4c876e7e89c02c9c08d7aa655470a98
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2015-03-28 12:30:43 -0400

    add notifications and version updates back in

--- webapp/static/js/tabula.js
+++ webapp/static/js/tabula.js
@@ -71,8 +71,11 @@ var TabulaRouter = Backbone.Router.extend({
 
 
 Tabula.getVersion = function(){
+  Tabula.notification = new Backbone.Model({});
+  Tabula.new_version = new Backbone.Model({});
   $.getJSON("/version", function(data){
     Tabula.api_version = data["api"];
+    Tabula.getNotifications();
 
     // if(Tabula.api_version.slice(0,3) == "rev"){
     //   $('#dev-mode-ribbon').show();
@@ -80,6 +83,53 @@ Tabula.getVersion = function(){
 
   })
 }
+Tabula.getNotifications = function(){
+  if(localStorage.getItem("tabula-notifications") === false) return;
+  $.get('https://api.github.com/repos/tabulapdf/tabula/releases',
+       function(data) {
+         if (data.length < 1) return;
+         if (Tabula.api_version.indexOf('rev') == 0) return;
+         // check if new version
+         var i = data.map(function(d) { return d.name; }).indexOf(Tabula.api_version);
+         // if index >= 1, current release is not the newest
+         if (i == 0) return;
+         var new_release = data[0];
+         if(new_release){
+            Tabula.new_version.set(new_release);
+         }
+     });
+  $.ajax({
+    url: 'http://tabula.jeremybmerrill.com/tabula/notifications.jsonp', 
+    dataType: "jsonp",
+    jsonpCallback: 'notifications',
+    success: function(data){
+      if(data.length < 1) return;
+
+      // find the first listed notification where today is between its `live_date` and `expires_date`
+      // and within the `versions` list.
+      // we might use this for, say, notifying users if a version urgently needs an update or something
+      // 
+      var notifications = $.grep(data, function(d){
+        var today = new Date();
+        if ( (d.expires_date && (new Date(d.expires_date) < today)) || (d.live_date && (new Date(d.live_date) > today)) ){ 
+          return false;
+        }
+        if( d.versions && d.versions.length > 0){
+          return (d.versions.indexOf(Tabula.api_version) > -1);
+        }else{
+          return true;
+        }
+      });
+
+      if(notifications.length >= 1){
+        console.log(notifications.length + " matching notifications:", notifications);
+        Tabula.notification.set(notifications[0]);
+      }else{
+        console.log("no notifications")
+      }
+    }});
+}
+
 
 $(function(){
   Tabula.getVersion();
