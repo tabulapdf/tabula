3cc98bbcf52c7726daca4e02d1e56df87c2fb13a
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2015-05-29 13:53:08 -0400

    tries to fix #319; also adds compass config that shoulda been in here for literally months

---
+++ config.rb
@@ -0,0 +1,25 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+# Set this to the root of your project when deployed:
+http_path = "webapp/"
+css_dir = "webapp/static/css"
+sass_dir = "webapp/static/sass"
+images_dir = "webapp/static/img"
+javascripts_dir = "webapp/static/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
