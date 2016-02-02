7de1120a665fc01ed507a712a9e0d8b019e7f79e
Manuel Aristaran <jazzido@jazzido.com>
2016-02-02 14:00:29 -0500

    Implmement new table detector (https://github.com/tabulapdf/tabula-java/pull/56)

--- webapp/tabula_web.rb
+++ webapp/tabula_web.rb
@@ -9,7 +9,7 @@ require 'tempfile'
 require 'fileutils'
 require 'securerandom'
 
-require_relative '../lib/jars/tabula-extractor-0.7.4-SNAPSHOT-jar-with-dependencies.jar'
+require_relative '../lib/jars/tabula-0.8.0-jar-with-dependencies.jar'
 
 require_relative '../lib/tabula_java_wrapper.rb'
 java_import 'java.io.ByteArrayOutputStream'
