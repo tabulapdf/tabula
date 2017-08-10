574272d47ffd9a7aa6a1e2795268be12b7042a84
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2017-08-10 09:11:44 -0400

    rename a variable for clarity

--- webapp/static/js/pdf_view.js
+++ webapp/static/js/pdf_view.js
@@ -881,7 +881,7 @@ Tabula.ControlPanelView = Backbone.View.extend({ // only one
 
                   // three states: autodetection still incomplete, autodetection done but no tables found, autodetection done and tables found
                   'restore_detected_tables': this.pdf_view.hasAutodetectedTables ? "autodetect-finished" : "autodetect-in-progress",
-                  'disable_detected_tables': numOfSelectionsOnPage > 0 || this.pdf_view.pdf_document.autodetected_selections.size() === 0 ? 'disabled="disabled"' : '',
+                  'disabled_if_there_are_selections': numOfSelectionsOnPage > 0 || this.pdf_view.pdf_document.autodetected_selections.size() === 0 ? 'disabled="disabled"' : '',
 
                   'disable_save_template':numOfSelectionsOnPage == 0 ? 'disabled="disabled"' : '',
                   'disable_load_template': numOfSelectionsOnPage > 0 ? 'disabled="disabled"' : ''
