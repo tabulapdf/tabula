db3f548eefb263070adbb43f7fa450599035f0a3
Shirley <slmendez@ncsu.edu>
2017-11-16 21:48:55 -0500

    Going back to last commit before merge
    Had issues merging back end and front end branches together, so I'm going back to the last commit.

---
+++ 484_P7_1-GUI/lib/tabula_job_executor/jobs/generate_thumbnails.rb
@@ -0,0 +1,20 @@
+require_relative '../executor.rb'
+require_relative '../../thumbnail_generator.rb'
+
+class GenerateThumbnailJob < Tabula::Background::Job
+  # args: (:file, :output_dir, :thumbnail_sizes, :page_index_job_uuid)
+
+  def perform
+
+    file_id = options[:file_id]
+    upload_id = self.uuid
+    filepath = options[:filepath]
+    output_dir = options[:output_dir]
+    thumbnail_sizes = options[:thumbnail_sizes]
+
+    generator = PDFBoxThumbnailGenerator.new(filepath, output_dir, file_id, thumbnail_sizes)
+    generator.add_observer(self, :at)
+    generator.generate_thumbnails!
+
+  end
+end
