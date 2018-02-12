4b61ed1e2d64dfbc1708dae9cd6dc4215995555a
Shirley <slmendez@ncsu.edu>
2018-02-11 23:51:12 -0500

    Test additions, the tests may be failing for now and addition of class name for the title in the Saved Templates page

--- src/test/java/TestHelpPage.java
+++ src/test/java/TestHelpPage.java
@@ -1,16 +1,25 @@
-import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestHelpPage {
     @Test
-    public void startWebDriver() throws InterruptedException {
+    public static void main(String[] args){
         WebDriver driver = new FirefoxDriver();
-        driver.navigate().to("http://127.0.0.1:9292/");
-        Assert.assertTrue("title should start with Selenium Simplified" ,
-                driver.getTitle().startsWith("Tabula"));
-        driver.quit();
+        driver.get("http://127.0.0.1:9292/");
+        try{
+            driver.findElement(By.id("upload-nav")).click();
+            Thread.sleep(3000);
+            System.out.print("pressed the button!");
+            driver.findElement(By.id("templates-nav")).click();
+            Thread.sleep(3000);
+            driver.navigate().back();
+            Thread.sleep(3000);
+            driver.quit();
+        }catch(Exception e){
+            System.out.print(e);
+        }
 
     }
 }
