5e8b54e7df625f504301c34ec4b3e86cc6c79f42
Shirley <slmendez@ncsu.edu>
2018-03-13 15:12:31 -0400

    Added on pathname of the geckodriver in one of the test case files

--- src/test/java/WatchmanTest.java
+++ src/test/java/WatchmanTest.java
@@ -1,27 +1,3 @@
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
-
 public class WatchmanTest {
-    private static String watchedLog;
 
-    @Rule
-    public TestWatcher watchman = new TestWatcher() {
-        @Override
-        protected void succeeded(Description description) {
-            watchedLog += description + " " + "success!\n";
-        }
-
-        @Override
-        protected void failed(Throwable e, Description description) {
-            watchedLog += description + "\n";
-        }
-    };
-    @Test
-    public void fails(){
-        fails();
-    }
-    @Test
-    public void succeeds(){}
 }
