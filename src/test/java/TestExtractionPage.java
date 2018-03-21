fa6991d467caafc4f31ee7e39fd87359cbefe00d
Shirley <slmendez@ncsu.edu>
2018-03-20 20:32:27 -0400

    Changed for all test cases so far to use ChromeDriver as opposed to FirefoxDriver, added on a better solution for calling the pdf file in order to utilize it in the test cases, as well as being able to delete the file once done.

--- src/test/java/TestExtractionPage.java
+++ src/test/java/TestExtractionPage.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,49 +17,54 @@ import static junit.framework.TestCase.assertTrue;
 // What it doesn't test are the individual URL links in the regex tabs, since those same links are already tested in
 // the TestHelpPage and TestHomePage test cases, as well as the Autodetect Tables button and the Preview & Export Data
 // button are not tested since their functionality will be tested in other test cases.
-// TODO: currently, I do not know how to directly call a pdf file so I can use it for the test cases without manually
-//  using the windows explorer to retrieve it. For now, I will use the pdf file that has been pre-uploaded already,
-// until this is fixed.
-// For this test case, I will use the eu_002.pdf file since I just need a way to get to the page and a pdf file is necessary.
+// TODO: currently, A better way to call the pdf has been changed; need to find a way to not use the absolute pathname
+// For this test case, eu_002.pdf is utilized.
 // @author SM modified: 3/6/18
 public class TestExtractionPage {
     WebDriver driver;
     @Test
     public void startWebDriver() throws InterruptedException{
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         driver.get("http://127.0.0.1:9292/");
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, 100);
-
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        String filePath = "/home/slmendez/484_P7_1-GUI/src/test/eu-002.pdf"; //
+        WebElement chooseFile = driver.findElement(By.id("file"));
+        chooseFile.sendKeys(filePath);
+        Thread.sleep(1000);
+        WebElement import_btn = driver.findElement(By.id("import_file"));
+        import_btn.click();
         try{
             //navigates to the extraction page and checks that it is in the extraction page
             By extract_name = By.linkText("Extract Data");
-            WebElement extract_button = wait.until(ExpectedConditions.visibilityOfElementLocated(extract_name));
-            Thread.sleep(2000);
+            WebElement extract_button = wait.until(ExpectedConditions.elementToBeClickable(extract_name));
             extract_button.click();
-            driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
 
             //menu options did not fully load
-            if(driver.findElements( By.id("restore-detected-tables") ).size() == 0){
+            if(driver.findElements( By.id("restore-detected-tables")).size() == 0){
                 //refresh the page
                 driver.navigate().refresh();
             }
-
+            if(driver.findElements(By.id("thumbnail-list")).size() == 0){
+                //refresh the page
+                driver.navigate().refresh();
+            }
             String regex_options_string = "Regex Options";
             By regex_options_title = By.id("regex_options_title");
             WebElement regex_options = wait.until(ExpectedConditions.visibilityOfElementLocated(regex_options_title));
-            driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(200, TimeUnit.SECONDS);
             assertTrue("Failed, couldn't find Extraction page", regex_options_string.equals(regex_options.getText()));
-
+            //Thread.sleep(5000);
             //checking that the PDF outline sidebar is visible
             By sidebar_title = By.id("sidebar");
             driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
-            Thread.sleep(2000);
+            //Thread.sleep(2000);
             assertTrue("PDF sidebar is not visible in Extraction page", driver.findElement(sidebar_title).isDisplayed());
             //clicking PDF outline button and checking if sidebar is not visible
             By pdf_outline_id = By.id("pdf_outline_title");
             WebElement pdf_outline_button = wait.until(ExpectedConditions.visibilityOfElementLocated(pdf_outline_id));
-            Thread.sleep(2000);
+            //Thread.sleep(2000);
             pdf_outline_button.click();
             By sidebar_check = By.id("sidebar");
             driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
@@ -86,11 +91,13 @@ public class TestExtractionPage {
             regex_options_button.click();
             driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
             assertFalse("Regex Options sidebar is visible in Extraction page", regex_command_string.equals(regex_command.getText()));
+           // Thread.sleep(2000);
 
             //waits for the templates button and then clicks on it, and checks that the templates content appears
+            driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
             By templates_name = By.id("templates_title");
-            WebElement templates_button = wait.until(ExpectedConditions.visibilityOfElementLocated(templates_name));
-            Thread.sleep(2000);
+            WebElement templates_button = wait.until(ExpectedConditions.elementToBeClickable(templates_name));
+          //  Thread.sleep(2000);
             templates_button.click();
             String templates_list_string = "Load templates:";
             By templates_list_title = By.id("loaded_templates_title");
@@ -98,7 +105,13 @@ public class TestExtractionPage {
             driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
             assertTrue("Failed, couldn't find Templates List in Extraction page", templates_list_string.equals(templates_list.getText()));
 
-            
+            //navigates back and deletes the pdf utilized
+            driver.navigate().back();
+            By delete_pdf = By.id("delete_pdf");
+            WebElement delete_btn = wait.until(ExpectedConditions.elementToBeClickable(delete_pdf));
+            delete_btn.click();
+            driver.switchTo().alert().accept();
+
         }catch(Exception e){
             System.out.print(e);
         }
