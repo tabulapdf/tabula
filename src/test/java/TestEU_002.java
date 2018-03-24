ac62b5c4318d4316d39ea961fc8705d5fe86ba2b
Shirley <slmendez@ncsu.edu>
2018-03-24 01:17:08 -0400

    Added on some beginning test cases for eu-002.pdf, testing for the regex search disabled button

--- src/test/java/TestEU_002.java
+++ src/test/java/TestEU_002.java
@@ -1,4 +1,5 @@
-import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 //Test of the eu_002 pdf file.
 // TODO: currently, I do not know how to directly call a pdf file so I can use it for the test cases without manually
@@ -18,34 +20,68 @@ import static junit.framework.TestCase.assertTrue;
 // -
 
 public class TestEU_002 {
-    WebDriver driver;
-    @Test
-    public void startWebDriver() throws InterruptedException {
+    public static WebDriver driver;
+    public static String Tabula_url = "http://127.0.0.1:9292/";
+    public WebDriverWait wait = new WebDriverWait(driver, 100);
+
+    @BeforeClass
+    public static void SetUp() throws InterruptedException {
         driver = new ChromeDriver();
-        driver.get("http://127.0.0.1:9292/");
+        driver.get(Tabula_url);
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, 100);
         String filePath = "/home/slmendez/484_P7_1-GUI/src/test/eu-002.pdf"; //
         WebElement chooseFile = driver.findElement(By.id("file"));
         chooseFile.sendKeys(filePath);
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         WebElement import_btn = driver.findElement(By.id("import_file"));
         import_btn.click();
         Thread.sleep(1000);
 
+    }
+    @Test
+    public void TestHalfRegexInputs() throws InterruptedException{
         try {
-
+            //navigates to the extraction page and checks that it is in the extraction page
             By extract_name = By.linkText("Extract Data");
-            WebElement extract_button = wait.until(ExpectedConditions.visibilityOfElementLocated(extract_name));
+            WebElement extract_button = wait.until(ExpectedConditions.elementToBeClickable(extract_name));
             extract_button.click();
+            driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
+            //menu options did not fully load
+            if(driver.findElements( By.id("restore-detected-tables")).size() == 0){
+                //refresh the page
+                driver.navigate().refresh();
+            }
+            if(driver.findElements(By.id("thumbnail-list")).size() == 0){
+                //refresh the page
+                driver.navigate().refresh();
+            }
+            //checks if it is in the extraction page
             String regex_options_string = "Regex Options";
             By regex_options_title = By.id("regex_options_title");
             WebElement regex_options = wait.until(ExpectedConditions.visibilityOfElementLocated(regex_options_title));
             driver.manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
             assertTrue("Failed, couldn't find Extraction page", regex_options_string.equals(regex_options.getText()));
 
+            //Test that checks that the regex search button is disabled after entering "Table 5" in pattern_before and
+            // clicking the regex search button
             By pattern_before_input = By.id("pattern_before");
             driver.findElement(pattern_before_input).sendKeys("Table 5");
+            By regex_search_id = By.id("regex-search");
+            assertFalse("Failed, regex search button is enabled", driver.findElement(regex_search_id).isEnabled());
+            driver.findElement(pattern_before_input).clear();
+
+            //Test that checks that the regex search button is disabled after entering "Table 6" in pattern_after and
+            // clicking the regex search button
+            By pattern_after_input = By.id("pattern_after");
+            driver.findElement(pattern_after_input).sendKeys("Table 6");
+            By regex_search_id2 = By.id("regex-search");
+            assertFalse("Failed, regex search button is enabled", driver.findElement(regex_search_id2).isEnabled());
+            driver.findElement(pattern_after_input).clear();
+
+
+
+
+/*
             By pattern_after_input = By.id("pattern_after");
             driver.findElement(pattern_after_input).sendKeys("Table 6");
             Thread.sleep(2000);
@@ -65,14 +101,19 @@ public class TestEU_002 {
             Thread.sleep(4000);
             WebElement search_button2 = wait.until(ExpectedConditions.visibilityOfElementLocated(search_id));
             search_button2.click();
-            Thread.sleep(3000);
+            Thread.sleep(3000); */
 
-
+            //navigates back and deletes the pdf utilized
+            driver.navigate().back();
+            By delete_pdf = By.id("delete_pdf");
+            WebElement delete_btn = wait.until(ExpectedConditions.elementToBeClickable(delete_pdf));
+            delete_btn.click();
+            driver.switchTo().alert().accept();
         } catch (Exception e) {
             System.out.print(e);
         }
     }
-    @After
-    public void TearDown(){
+    @AfterClass
+    public static void TearDown(){
         driver.quit();
     }}
