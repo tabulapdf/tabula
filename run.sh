9b387e6d371837139d468fee84b79cf6c04e385a
Shirley <slmendez@ncsu.edu>
2018-04-10 11:44:06 -0400

    Moved a lot of the commands to the run.sh file to see if that will make a difference

--- run.sh
+++ run.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
-pwd
-jruby -G -r jbundler -S rackup
+# download and use jruby
+wget https://repo1.maven.org/maven2/org/jruby/jruby-dist/9.1.16.0/jruby-dist-9.1.16.0-bin.zip
+unzip jruby-dist-9.1.16.0-bin.zip
+mv jruby-9.1.16.0 ~/
+export PATH=~/jruby-9.1.16.0/bin:$PATH
+# create back-end jar
+cd ~/
+git clone https://github.com/redmyers/484_P7_1-Java.git
+cd 484_P7_1-Java
+git checkout Header_Back_End
+mvn clean compile assembly:single
+mvn install:install-file -Dfile=target/tabula-1.0.2-SNAPSHOT-jar-with-dependencies.jar -DgroupId=technology.tabula -DartifactId=tabula -Dversion=1.0.2-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
+# start server via GUI repo
+cd ~/
+git clone https://github.com/redmyers/484_P7_1-GUI.git
+cd 484_P7_1-GUI
+git checkout Header_Addition
+export PATH=~/jruby-9.1.16.0/bin:$PATH
+gem install bundler
+bundle install
+jruby -S jbundle installjruby -G -r jbundler -S rackup
 
