4db635257197848f0ffa752c58709cc876b5077a
Ross Myers <remyers@nsu.edu>
2018-04-10 16:35:36 -0400

    Tweaking waitForServerLoad a bit...

--- waitForServerLoad.sh
+++ waitForServerLoad.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 gem update --system
-curl http::localhost:9292
-res=$?
-echo res
+res=1;
+while "$res" -ne 0
+do
+ curl http::localhost:9292
+ res=$?
+ echo "Return value of curl..."
+ echo res
+done
 
 exit $res
 
