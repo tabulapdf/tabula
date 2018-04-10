3dfe329f5c75db057cc7062c1d4e545ffccb3072
Ross Myers <remyers@nsu.edu>
2018-04-10 17:18:40 -0400

    Giving the server more time to load...

--- waitForServerLoad.sh
+++ waitForServerLoad.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 gem update --system
 res=1;
-iter=10;
+iter=20;
 while [[ "$res" -ne 0 ]] && [[ "$iter" -gt 0 ]]
 do
  iter=$((iter-1));
@@ -9,7 +9,7 @@ do
  curl -s http://localhost:9292/
  res=$?
  printf "Return value of curl:%d\n" "$res"
- sleep 5s #Sleep for 5 seconds in between curl calls...
+ sleep 30s #Sleep for 5 seconds in between curl calls...
 done
 
 if [[ "$res" -ne 0 ]]
