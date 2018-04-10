ebdfc748032774005dd82c29977b2f7369b407c5
Ross Myers <remyers@nsu.edu>
2018-04-10 17:14:15 -0400

    OK, this SHOULD not have any syntax errors...not holding my breath..

--- waitForServerLoad.sh
+++ waitForServerLoad.sh
@@ -2,15 +2,20 @@
 gem update --system
 res=1;
 iter=10;
-while "$res" -ne 0 && $"iter" -ne 0
+while [[ "$res" -ne 0 ]] && [[ "$iter" -gt 0 ]]
 do
- curl http::localhost:9292
+ iter=$((iter-1));
+ printf "Iteration #%d\n" "$((10-$iter))"
+ curl -s http://localhost:9292/
  res=$?
- printf "Return value of curl:%d","$res"
+ printf "Return value of curl:%d\n" "$res"
  sleep 5s #Sleep for 5 seconds in between curl calls...
- iter=$iter-1;
- printf "Iteration #:%d","$iter"
 done
+
+if [[ "$res" -ne 0 ]]
+then
+  printf "Connection could not be made to server...\n"
+fi
 
 exit $res
 
