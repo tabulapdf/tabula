0aa639d88d34cbd0792288dbce258a2450ec6332
Ross Myers <remyers@nsu.edu>
2018-04-11 19:12:22 -0400

    Trying to figure out what exactly the mvn install is needed for...

--- waitForServerLoad.sh
+++ waitForServerLoad.sh
@@ -5,12 +5,12 @@ num_iter=100
 iter="$num_iter";
 while [[ "$res" -ne 0 ]] && [[ "$iter" -gt 0 ]]
 do
+ sleep 1m #Sleep for a minute in between curl calls...
  iter=$((iter-1));
  printf "Iteration #%d\n" "$(($num_iter-$iter))"
  curl -s http://localhost:9292/
  res=$?
  printf "Return value of curl:%d\n" "$res"
- sleep 1m #Sleep for a minute in between curl calls...
 done
 
 if [[ "$res" -ne 0 ]]
