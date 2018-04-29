2431571a91e02625a6a2031cde90b9cbb6e2a6e6
Shirley <slmendez@ncsu.edu>
2018-04-29 01:33:08 -0400

    Updated run.sh file to clone the new repo

--- run.sh
+++ run.sh
@@ -8,12 +8,11 @@ export PATH=~/jruby-9.1.16.0/bin:$PATH
 # create back-end jar
 echo 'CLONING BACK END REPO...'
 cd ~/
-git clone https://github.com/redmyers/484_P7_1-Java.git
-cd 484_P7_1-Java
-git checkout Up_To_This_Point
+git clone https://github.com/redmyers/tabula-java.git
+cd tabula-java
 echo 'CREATING BACK-END JAR...'
 mvn clean compile assembly:single
-mvn install:install-file -Dfile=target/tabula-1.0.2-SNAPSHOT-jar-with-dependencies.jar -DgroupId=technology.tabula -DartifactId=tabula -Dversion=1.0.2-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
+mvn install:install-file -Dfile=../tabula-java/target/tabula-1.0.2-SNAPSHOT-jar-with-dependencies.jar -DgroupId=technology.tabula -DartifactId=tabula -Dversion=1.0.2-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
 # start server via GUI repo
 cd ~/
 git clone https://github.com/redmyers/484_P7_1-GUI.git
